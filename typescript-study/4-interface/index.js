4866baee58231e4bd17ae5857f65d1d2104216bb
ngthuongdoan <ngthuongdoan@gmail.com>
2021-04-10 21:03:10 +0700

    change to Max course

--- typescript-study/4-interface/index.js
+++
@@ -1,6 +0,0 @@
-console.log("%cInterface", "color:white;font-weight:bold;font-size:4rem;-webkit-text-stroke: 1px black");
-function xemtt(p) {
-    console.log("Xin chao " + p.ten + " ban " + p.tuoi + " phai khong");
-}
-xemtt({ tuoi: 18, ten: 'ahihi' });
-console.log("%cAbstract", "color:white;font-weight:bold;font-size:4rem;-webkit-text-stroke: 1px black");
