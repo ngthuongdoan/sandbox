f187fc62a86b4144f62f95de3665dbba3986c13b
ngthuongdoan <ngthuongdoan@gmail.com>
2021-06-11 16:00:49 +0700

    delete tasks

---
+++ angular-study/src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faTimes, fas } from '@fortawesome/free-solid-svg-icons';
+
+import { Task } from '../../Task';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.scss'],
+})
+export class TaskItemComponent implements OnInit {
+  @Input() task!: Task;
+  @Output() deleteTask: EventEmitter<Task> = new EventEmitter();
+  faTimes = faTimes;
+
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas);
+    library.addIcons(faTimes);
+  }
+  ngOnInit(): void {}
+  onDelete(task: Task): void {
+    this.deleteTask.emit(task);
+  }
+}
